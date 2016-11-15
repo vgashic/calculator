d338dc2fe60293e0a9b90695f98f9ba25c41b9c7
Vladimir Gasic <vgashic@gmail.com>
2016-11-15 01:19:53 +0100

    Added sass task and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,14 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function () {
-	return gulp.src("app/scss/styles.scss")
+	return gulp.src("app/scss/**/*.scss")
 		.pipe(sass()) // Using gulp-sass
 		.pipe(gulp.dest("app/css"))
+});
+
+// gulp.watch("app/scss/**/*.scss", ["sass"]);
+
+gulp.task("watch", function () {
+	// sass watcher
+	gulp.watch("app/scss/**/*.scss", ["sass"]);
 });
